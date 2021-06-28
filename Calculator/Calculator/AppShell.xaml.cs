6bf001aff318297f2319e0b979137ebeac886174
Mayank Kumar <kmayank38@gmail.com>
2021-06-28 19:46:42 +0530

    Add project files.

---
+++ Calculator/Calculator/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Calculator.ViewModels;
+using Calculator.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Calculator/Calculator/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Calculator.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Calculator.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
